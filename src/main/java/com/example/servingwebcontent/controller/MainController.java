3078ec707f116b002669159525fced8f13d0087c
Dmitry <dima.game2010@yandex.ru>
2020-08-15 21:56:56 +0300

    4.1 урок) связи в БД

--- src/main/java/com/example/servingwebcontent/controller/MainController.java
+++ src/main/java/com/example/servingwebcontent/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.servingwebcontent.controller;
 
 import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.domain.User;
 import com.example.servingwebcontent.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,8 +31,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Model model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Model model){
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
